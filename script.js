d05635f0b6092247dd79ce5cf2721666f67df5c7
Linus Johansson <lsjn88@gmail.com>
2021-12-25 12:56:18 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+/*
+Write a function, stairmaster(n), that will compute the number of ways to climb a flight of n steps, taking 1, 2, or 3 steps at a time.
+
+Take the example of climbing n = 4 steps. There are seven different ways one can climb four stairs using 1, 2, or 3 steps at a time: [1,1,1,1] [2,1,1] [1,2,1] [1,1,2] [2,2] [1,3] [3,1].
+
+Make sure to find all permutations, not combinations, as the order matters. Climbing one step then two steps is different from climbing two steps then one step.
+*/
+
+function stairmaster(n) {
+  
+}
+
+console.log(stairmaster(4));
+
+// Leave this so we can test your code:
+module.exports = stairmaster;
